67c91dd3dda3f175bbce81e96acecaa58a4fe0cf
menglijinghua <508304482@qq.com>
2020-09-03 15:40:49 +0800

    all

--- 4/test.m
+++ 4/test.m
@@ -1,8 +1,9 @@
-global x
-x=3;
-disp(func());
-function x=func()
-    global x
-    disp(x)
-    x=5;
-end
+% global x
+% x=3;
+% disp(func());
+% function x=func()
+%     global x
+%     disp(x)
+%     x=5;
+% end
+normrnd(15,1)
