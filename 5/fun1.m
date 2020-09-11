20c40f12f7cf46ecc5fede2651b7986d10f3a158
menglijinghua <508304482@qq.com>
2020-09-11 19:53:00 +0800

    all

--- 5/fun1.m
+++ 5/fun1.m
@@ -20,26 +20,31 @@ for j=1:T-1
     elseif judge==2
         T0=175;
     elseif judge==3
-        T0=175+4*j*dt;
+        T0=175+(14/3)*j*dt;
     elseif judge==4
         T0=195;
     elseif judge==5
-        T0=195+8*j*dt;
+        T0=195+(28/3)*j*dt;
     elseif judge==6
         T0=235;
     elseif judge==7
-        T0=235+4*j*dt;
+        T0=235+(14/3)*j*dt;
     elseif judge==8
         T0=255;
     elseif judge==9
-        T0=255-46*j*dt;
+        T0=255-(161/3)*j*dt;
     elseif judge==10
         T0=25;
     elseif judge==11
         T0=25;
     end
-    U(1,j+1)=(dx*T0+w*U(2,j+1))/(dx+w);
-    U(X,j+1)=(dx*T0+w*U(X-1,j+1))/(dx+w);
+    if judge==11||judge==12
+        U(1,j+1)=25+(U(1,j)-25)*exp(1)^(-dt/w);
+        U(X,j+1)=25+(U(X,j)-25)*exp(1)^(-dt/w);
+    else
+        U(1,j+1)=(dx*T0+w*U(2,j+1))/(dx+w);
+        U(X,j+1)=(dx*T0+w*U(X-1,j+1))/(dx+w);
+    end
 end
 U0=U(:,T);
 R0=U(floor(X/2),1:500:end);
--- 5/run1.m
+++ 5/run1.m
@@ -5,8 +5,10 @@ Data=xlsread('附件.xlsx');
 P=Data(:,2);
 error_=inf;
 W=0;LAMBDA=0;
-for w=1e-5:1e-4:1e-2
-    for lambda=0.01:0.01:0.5
+w1=1e-3;w2=1e-4;
+lambda1=0.16;lambda2=0.16;
+for w=1e-4
+    for lambda=0.16
         [U1,R1]=fun1(25,25,1,w,lambda);
         [U2,R2]=fun1(5*smallZones+4*interval,U1,2,w,lambda);
         [U3,R3]=fun1(interval,U2,3,w,lambda);
@@ -16,10 +18,10 @@ for w=1e-5:1e-4:1e-2
         [U7,R7]=fun1(interval,U6,7,w,lambda);
         [U8,R8]=fun1(2*smallZones+interval,U7,8,w,lambda);
         [U9,R9]=fun1(interval,U8,9,w,lambda);
-        [U10,R10]=fun1(2*smallZones+interval,U9,10,w,lambda);
-        [U11,R11]=fun1(25,U10,11,w,lambda);
+        [U10,R10]=fun1(2*smallZones+interval,U9,10,w1,lambda1);
+        [U11,R11]=fun1(25,U10,11,w2,lambda2);
         R=[R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11];
-        R(1:22)=[];
+        R(1:38)=[];
         len=min(length(P),length(R));
         error=0;
         for i=1:len
@@ -32,3 +34,6 @@ for w=1e-5:1e-4:1e-2
         end
     end
 end
+plot(R,'r');
+hold on
+plot(P,'b');
